7da37dc90b5e0524ed1f51bd5dee64630624a203
Volodymyr <volodymyrfilon1312@gmail.com>
2023-10-30 19:32:14 +0100

    service: get marvel data promise

---
+++ src/services/MarvelService.js
@@ -0,0 +1,24 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=3155e7e4773051f4be08caea81d21383';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
