8d37498389cc8ae05a99dd7bfeebcd30d2e65349
Volodymyr <volodymyrfilon1312@gmail.com>
2023-11-08 17:47:33 +0100

    handle load btn

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -14,6 +14,7 @@ class CharList extends Component {
         error: false,
         newItemLoading: false,
         offset: 210,
+        charEnded: false,
     }
 
     marvelService = new MarvelService();
@@ -36,11 +37,17 @@ class CharList extends Component {
     }
 
     onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true;
+        }
+
         this.setState(({offset, charList}) => ({
             charList: [...charList, newCharList],
             loading: false,
             newItemLoading: false,
-            offset: offset + 9
+            offset: offset + 20000,
+            charEnded: ended
         }))
     }
 
@@ -78,7 +85,7 @@ class CharList extends Component {
 
     render() {
 
-        const { charList, loading, error, offset, newItemLoading } = this.state;
+        const { charList, loading, error, offset, newItemLoading, charEnded } = this.state;
 
         const items = this.renderItems(charList);
 
@@ -94,6 +101,7 @@ class CharList extends Component {
                 <button 
                     className="button button__main button__long"
                     disabled={newItemLoading}
+                    style={{display: charEnded ? 'none' : 'block'}}
                     onClick={() => this.onRequest(offset)}
                 >
                     <div className="inner">load more</div>
