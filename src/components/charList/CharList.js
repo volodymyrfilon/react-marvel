395e9e21c38f94c77ad4fa783773b6850bdfd6d3
Volodymyr <volodymyrfilon1312@gmail.com>
2023-12-11 16:08:55 +0100

    fix: charlist notfound style

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -49,7 +49,9 @@ const CharList = props => {
 			let imgStyle = { objectFit: 'cover' }
 			if (
 				item.thumbnail ===
-				'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+					'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ||
+				item.thumbnail ===
+					'http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708.gif'
 			) {
 				imgStyle = { objectFit: 'unset' }
 			}
