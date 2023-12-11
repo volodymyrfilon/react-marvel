ce2e02679729ef736fef242c338266e91f04315f
Volodymyr <volodymyrfilon1312@gmail.com>
2023-12-11 16:07:11 +0100

    feat: marvel comics

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,7 +2,6 @@ import { useHttp } from '../hooks/http.hook'
 
 const useMarvelService = () => {
 	const { loading, request, error, clearError } = useHttp()
-
 	const _apiBase = 'https://gateway.marvel.com:443/v1/public/'
 	const _apiKey = 'apikey=3155e7e4773051f4be08caea81d21383'
 	const _baseOffset = 210
@@ -19,6 +18,18 @@ const useMarvelService = () => {
 		return _transformCharacter(res.data.results[0])
 	}
 
+	const getAllComics = async (offset = 0) => {
+		const res = await request(
+			`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
+		)
+		return res.data.results.map(_transformComics)
+	}
+
+	const getComics = async id => {
+		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`)
+		return _transformComics(res.data.results[0])
+	}
+
 	const _transformCharacter = char => {
 		return {
 			id: char.id,
@@ -33,12 +44,31 @@ const useMarvelService = () => {
 		}
 	}
 
+	const _transformComics = comics => {
+		return {
+			id: comics.id,
+			title: comics.title,
+			description: comics.description || 'There is no description',
+			pageCount: comics.pageCount
+				? `${comics.pageCount} p.`
+				: 'No information about the number of pages',
+			thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+			language: comics.textObjects[0]?.language || 'en-us',
+			// optional chaining operator
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: 'not available',
+		}
+	}
+
 	return {
 		loading,
 		error,
 		clearError,
 		getAllCharacters,
 		getCharacter,
+		getAllComics,
+		getComics,
 	}
 }
 
