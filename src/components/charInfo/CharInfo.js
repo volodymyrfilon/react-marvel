21fa82f584cf065f27d6de9efaf746158bdce9b5
Volodymyr <volodymyrfilon1312@gmail.com>
2023-12-11 16:58:07 +0100

    fix: styles

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -52,7 +52,9 @@ const View = ({ char }) => {
 	let imgStyle = { objectFit: 'cover' }
 	if (
 		thumbnail ===
-		'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+			'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ||
+		thumbnail ===
+			'http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708.gif'
 	) {
 		imgStyle = { objectFit: 'contain' }
 	}
