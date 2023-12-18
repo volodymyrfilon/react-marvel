ad199e9a582640a5e3aec91d501ab7ae8934b3c0
Volodymyr <volodymyrfilon1312@gmail.com>
2023-12-18 17:57:56 +0100

    fix: route

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -5,7 +5,7 @@ const AppHeader = () => {
 	return (
 		<header className='app__header'>
 			<h1 className='app__title'>
-				<Link to='/'>
+				<Link to='/react-marvel'>
 					<span>Marvel</span> information portal
 				</Link>
 			</h1>
@@ -17,7 +17,7 @@ const AppHeader = () => {
 							style={({ isActive }) => ({
 								color: isActive ? '#9f0013' : 'inherit',
 							})}
-							to='/'
+							to='/react-marvel'
 						>
 							Characters
 						</NavLink>
